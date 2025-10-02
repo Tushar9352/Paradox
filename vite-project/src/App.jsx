1627a0e740524e8d1c26b2cc14cc1bd20cb9d868
Tushar Saini <mail.honey6401@gmail.com>
2025-10-02 17:56:44 +0530

    Updates

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,4 +1,10 @@
+import Button from "./button";
 function App(){
-  return <h1>Hello Vite!</h1>
+  return (
+    <div>
+      <h1>Hello Vite!</h1>
+      <Button></Button>
+    </div>
+  );
 }
 export default App;
---
+++ vite-project/src/button.jsx
@@ -0,0 +1,9 @@
+function Button() {
+  return (
+    <div>
+      <button>Press here to Subscribe</button>
+    </div>
+  );
+}
+
+export default Button;
